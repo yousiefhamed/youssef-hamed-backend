e96f95613ce8610d37cb49df2e3cbbc2dc444ed5
Youssef Hamed <yousiefhamed@gmail.com>
2024-10-10 07:39:29 +0300

    adding api folder for deployment

--- api/server.js
+++ api/server.js
@@ -13,17 +13,22 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
+// Routes
+app.use("/api/contactForm", require("../routes/formRoutes"));
+// app.use("/api/contact", require("../routes/contact"));
+// app.use("/api/admin", require("../routes/admin"));
+
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-// Routes
-app.use("/api/contactForm", require("./routes/formRoutes"));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
